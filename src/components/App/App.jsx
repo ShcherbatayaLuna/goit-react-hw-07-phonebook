739f55c24f94e093419595673a013f39d6171e81
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-24 13:15:19 +0200

    Update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,47 +1,31 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContact, filterContact } from 'redux/ContactsSlice';
-import { nanoid } from 'nanoid';
+import { fetchContacts } from 'redux/utils/ContactsApi';
+import { selectContacts } from 'redux/selectors/Selectors';
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 import { Container, TitlePrimary, TitleSecondary } from './App.styled';
 
 export default function App() {
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
+  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
-  const formSubmitHandler = ({ name, number }) => {
-    const todo = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    dispatch(addContact(todo));
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <Container>
       <TitlePrimary>Phonebook</TitlePrimary>
-      <ContactForm onSubmit={formSubmitHandler} />
-
-      <TitleSecondary>Contacts</TitleSecondary>
-      <Filter
-        filter={filter}
-        onChange={event => dispatch(filterContact(event.target.value))}
-      />
-      <ContactList
-        contacts={getVisibleContacts()}
-        deleteContact={event => dispatch(deleteContact(event))}
-      />
+      <ContactForm />
+      {contacts && (
+        <>
+          <TitleSecondary>Contacts</TitleSecondary>
+          <Filter />
+          <ContactList />
+        </>
+      )}
     </Container>
   );
 }
